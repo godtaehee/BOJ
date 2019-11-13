1bd3ec1ad89a9daa972bf41a179b98f318dc2029
Taehee <44861205+wonderfulhuman@users.noreply.github.com>
2019-11-13 17:24:44 +0900

    Create 2439.cpp

---
+++ 2439.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	
+	int a;
+	
+	scanf("%d", &a);
+	
+	for(int i = 0; i < a; i++){
+		for(int j = 0; j < a - i - 1; j++){
+			printf(" ");
+		}
+		for(int z = 0; z <= i;  z++){
+			printf("*");	
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
