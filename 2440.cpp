2a700b65dd466dd048eb9ea16061944d47731b83
Taehee <44861205+wonderfulhuman@users.noreply.github.com>
2019-11-13 17:25:20 +0900

    Create 2440.cpp

---
+++ 2440.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	
+	int a;
+	
+	scanf("%d", &a);
+	
+	for(int i = 0; i < a; i++){
+		for(int j = 0; j < a - i; j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+}
