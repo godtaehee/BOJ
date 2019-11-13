b6149fc3d337073b646a0123f78d795620a348eb
Taehee <44861205+wonderfulhuman@users.noreply.github.com>
2019-11-13 17:27:57 +0900

    Create 2444.c

---
+++ 2444.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(){
+	
+	int a;
+	
+	scanf("%d", &a);
+	
+	for(int i = 0; i < a; i++){
+		
+		for(int j = 0; j < a - i - 1; j++){
+			printf(" ");
+		}
+		
+		for(int z = 0; z < 2 * i + 1; z++){
+			printf("*");
+		}
+
+		printf("\n");
+	}
+	
+	for(int i = 0; i < a; i++){
+	
+		for(int j = 0; j < i + 1; j++){
+			printf(" ");
+		}
+		
+		for(int z = 0; z <= 2 * (a - i - 2)  ; z++){
+			printf("*");
+		}
+		
+
+
+		printf("\n");
+	}	
+	
+}
