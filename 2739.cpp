56d1d2d2c1c750a06d66ccd1ffb07c13a68c0348
Taehee <44861205+wonderfulhuman@users.noreply.github.com>
2019-11-14 23:34:49 +0900

    Create 2739.cpp

---
+++ 2739.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+
+int main(){
+	int T;
+	scanf("%d", &T);
+
+	for(int j = 1; j <= 9; j++){
+		cout << T << " * " << j << " = " << T * j << endl;
+	}
+
+}
