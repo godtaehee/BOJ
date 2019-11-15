12f78b44d0126f24f091379eb4edf59f0abf912c
Taehee <44861205+wonderfulhuman@users.noreply.github.com>
2019-11-15 09:42:36 +0900

    Create 11720.cpp

---
+++ 11720.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+
+	int T;
+	int a;
+	int sum = 0;
+	
+	cin >> T;
+
+	while (T--) {
+		
+	scanf("%1d", &a);
+	sum += a;
+	
+	}
+	
+	cout << sum << endl;
+
+
+	return 0;
+}
