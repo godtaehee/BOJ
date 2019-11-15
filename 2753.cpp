56b72df0444056906dfe4208f2b03464d779a10e
Taehee <44861205+wonderfulhuman@users.noreply.github.com>
2019-11-15 09:09:49 +0900

    Create 2753.cpp

---
+++ 2753.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a;
+
+    scanf("%d", &a);
+
+    if(a % 4 == 0){
+        if(a % 100 == 0){
+            if(a % 400 == 0)
+                printf("1");
+            else
+                printf("0");
+        } else
+            printf("1");
+
+    } else
+        printf("0");
+    return 0;
+}
