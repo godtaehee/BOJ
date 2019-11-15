ed3d249e7cb2333da8706614d4c87d20339faac5
Taehee <44861205+wonderfulhuman@users.noreply.github.com>
2019-11-15 09:25:45 +0900

    Create 10171.cpp

---
+++ 10171.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    printf("\\    /\\\n");
+    printf(" )  ( ')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|\n");
+
+
+    return 0;
+}
