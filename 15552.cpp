76a5c124669e932ccee0967cfced3bb9020ee776
Taehee <44861205+wonderfulhuman@users.noreply.github.com>
2019-11-15 09:43:43 +0900

    Create 15552.cpp

---
+++ 15552.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+
+    int T;
+    int x,y;
+
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+
+    cin >> T;
+
+    while(T--){
+        cin >> x >> y;
+        cout << x + y << '\n';
+
+    }
+}
