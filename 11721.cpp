58f9b2533c88ed26f9f4ea24cd77b4ee0e98969e
Taehee <44861205+wonderfulhuman@users.noreply.github.com>
2019-11-15 09:43:13 +0900

    Create 11721.cpp

---
+++ 11721.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+
+    char s[100];
+		
+		
+		while(scanf("%10s", s) == 1){
+			cout << s << endl;
+		}
+
+
+    return 0;
+}
