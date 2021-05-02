ade8ac2c698ffc530abae307314832d6d5b419b4
Taehee Kim <44861205+godtaehee@users.noreply.github.com>
2021-05-02 23:18:41 +0900

    Create linkedlist.cpp

---
+++ linkedlist.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+const int maxL = 1000;
+int prev[maxL], nxt[maxL], data[maxL];
+int unused = 1;
+
+void insert(int addr, int num) {
+    
+}
+
+
+
+int main() {
+
+
+    return 0;
+}
